e350cfc70d270b0fe41c795fb5efa1ab6ea6c6f0
axu2026 <157142880+axu2026@users.noreply.github.com>
2025-02-12 19:25:49 -0500

    forgot the actual files

---
+++ HW2 Docker/flask/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, make_response, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    response = make_response(
+        {
+            'response': 'Hello, World!',
+            'status': 200
+        }
+    )
+    return response
+
+@app.route('/repeat')
+def repeat():
+    response = make_response(
+        {
+            "body": request.args.get('input'),
+            "status": 200
+        }
+    )
+    return response
+
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    response = make_response(
+        {
+            'body': 'OK',
+            'status': 200
+        }
+    )
+    return response
+
+@app.route('/hang')
+def hang():
+    while True:
+        continue
+
+if __name__ == '__main__':
+    # By default flask is only accessible from localhost.
+    # Set this to '0.0.0.0' to make it accessible from any IP address
+    # on your network (not recommended for production use)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT')), debug=True, threaded=False)
